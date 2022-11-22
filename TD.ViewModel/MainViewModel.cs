78d0338c31a5d63ff077a673ef2c560511eeaeb8
Felix <felix.bertagnolio8@etu.univ-lorraine.fr>
2022-11-22 16:38:18 +0100

    Ajoutez des fichiers projet.

---
+++ TD.ViewModel/MainViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TD
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _titre;
+        ObservableCollection<string> liste = new ObservableCollection<string>();
+        ObservableCollection<string> detail = new ObservableCollection<string>();
+
+        ObservableCollection<string> liste2 = new ObservableCollection<string>();
+        private int nb_piste = 0;
+
+        public MainViewModel()
+        {
+            _titre = "Appli Musique";
+            Liste();
+        }
+
+
+        public void Liste()
+        {
+            liste.Add(detail.Add());
+            liste.Add("John Lenon - Imagine");
+            //Console.WriteLine(liste);
+            OnPropertyChanged(nameof(liste));
+        }
+
+        public void Ajouter_gauche()
+        {
+            liste.Add("Artiste - Album");
+            OnPropertyChanged(nameof(liste));
+        }
+
+        public void Supprimer_gauche(int index)
+        {
+            liste.RemoveAt(index);
+            OnPropertyChanged(nameof(liste));
+        }
+
+        public void Ajouter_piste(String nm_piste)
+        {
+            String piste = nm_piste;
+            nb_piste++;
+            liste2.Add(nb_piste + " - " + piste);
+            OnPropertyChanged(nameof(liste2));
+        }
+
+        public void Supprimer_piste(int index)
+        {
+            liste2.RemoveAt(index);
+            OnPropertyChanged(nameof(liste));
+        }
+
+        public string Artiste()
+        {
+        }
+
+        // La liaison de données XAML ne reconnait que les propriétés.
+
+        public string Titre
+        {
+            get { return _titre; }
+        }
+
+        public ObservableCollection<String> Result_liste
+        {
+            get { return liste; }
+        }
+
+        public ObservableCollection<String> Result_liste2
+        {
+            get { return liste2; }
+        }
+
+        public string Compositeur
+        {
+
+        }
+
+    }
+}
---
+++ TD/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int index;
+        String nm_piste;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Définition de la vue-modèle par la fenêtre.
+            // Par convention, nous allons plutôt la définir
+            // au démarrage de l'application (classe App).
+            
+            // DataContext = new MainViewModel();
+
+            // Abonnement à un évènement
+            // Equivalent avec Loaded="MainWindow_Loaded" en XAML
+            //Loaded += MainWindow_Loaded;
+        }
+
+        MainViewModel vm = new MainViewModel();
+        
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MainViewModel vm)
+            {
+                vm.Ajouter_gauche();
+            }
+        }
+
+        private void button_Click1(object sender, RoutedEventArgs e)
+        {
+            index = List.SelectedIndex;
+            if (DataContext is MainViewModel vm)
+            {
+                vm.Supprimer_gauche(index);
+            }
+        }
+
+        private void button_Click2(object sender, RoutedEventArgs e)
+        {
+            nm_piste = Pist.Text;
+            if (DataContext is MainViewModel vm)
+            {
+                vm.Ajouter_piste(nm_piste);
+            }
+
+        }
+
+        private void button_Click3(object sender, RoutedEventArgs e)
+        {
+            index = List2.SelectedIndex;
+            if (DataContext is MainViewModel vm)
+            {
+                vm.Supprimer_piste(index);
+            }
+        }
+
+
+
+        // Déclenché lorsque la fenêtre est chargée.
+        /*private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MainViewModel vm)
+            {
+                artiste = List.SelectedItems.ToString();
+                //vm.Heure();
+            }
+        }*/
+
+        // Si l'on était abonné à l'évènement Click du bouton,
+        // à la place d'une commande :
+        //
+        // private void Button_Click(object sender, RoutedEventArgs e)
+        // {
+        //     if (DataContext is MainViewModel vm)
+        //     {
+        //         vm.Heure();
+        //     }
+        // }
+    }
+}
